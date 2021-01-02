af4272b5f4a0649901e3c8664c54f7e2965ea397
Carlos Brown <carlos.brown3@gmail.com>
2021-01-02 15:16:18 -0500

    Update analysis

--- PE_STUDY_WRANGLE.py
+++ PE_STUDY_WRANGLE.py
@@ -2,14 +2,25 @@ import pandas as pd
 from numpy import NaN
 import numpy as np
 
-df_saddle = pd.read_excel('PE Study- Final for Carlos 8-20-20.xlsx', sheet_name='Saddle PE')
-df_nosaddle = pd.read_excel('PE Study- Final for Carlos 8-20-20.xlsx', sheet_name='Non-Saddle PE')
+df_saddle = pd.read_csv('./objects/Saddle PE-Table 1.csv')
+df_nosaddle = pd.read_csv('./objects/Non-Saddle PE-Table 1.csv')
 
 # Data Cleaning
-
 # Rename columns
-df_nosaddle.rename(columns={' EKOS':'EKOS', 'tpA ':'tpA','Indentificaton':'ID', 'Hight-cm':'Height_cm', 'sPESI (low risk = 0; 1 = 1 or greater)':'sPESI'}, inplace=True)
-df_saddle.rename(columns={' EKOS':'EKOS', 'tpA ':'tpA','Indentificaton':'ID', 'Hight-cm':'Height_cm', 'sPESI (low risk = 0; 1 = 1 or greater)':'sPESI'}, inplace=True)
+df_nosaddle.rename(columns={' EKOS':'EKOS', 
+                            'tpA ':'tpA',
+                            'Indentificaton':'ID', 
+                            'Hight-cm':'Height_cm', 
+                            'sPESI (low risk = 0; 1 = 1 or greater)':'sPESI'}, 
+                            inplace=True)
+df_saddle.rename(columns={' EKOS':'EKOS', 
+                        'tpA ':'tpA',
+                        'Indentificaton':'ID', 
+                        'Hight-cm':'Height_cm', 
+                        'sPESI (low risk = 0; 1 = 1 or greater)':'sPESI', 
+                        'Shock (SBP <90)':'Shock (BP <90)',
+                        'PE Severity 1=low risk; 2=intermediate; 3=massive; 4= indeterminate':'PE Severity 1=low risk; 2=intermediate; 3=massive; 4=indeterminate'}, 
+                        inplace=True)
 
 # Remove whitespace from text borders
 def strip_text(item):
@@ -18,7 +29,7 @@ def strip_text(item):
 # Replace 'x' with more data friendly missing value
 def replacesymbol(item):
     if str(item).strip() == 'x' or str(item).strip() == '?':
-        return NaN
+        return np.nan
     else:
         return item
         
@@ -31,7 +42,6 @@ for col in saddle_cols:
 for col in nosaddle_cols:
     df_nosaddle[col] = df_nosaddle[col].apply(replacesymbol)
 
-
 # df_saddle['Patient Status at 30 days'] = df_saddle['Patient Status at 30 days'].apply(strip_text)
 # df_nosaddle['Patient Status at 30 days'] = df_nosaddle['Patient Status at 30 days'].apply(strip_text)
 
@@ -39,7 +49,12 @@ for col in nosaddle_cols:
 # Variables that qualify as adverse event include death, Arrhythmia needing treatment, Supplemental O2 required, Ionotropes, Vasopressors
 def adverse_event(row):
     adverse_event = 0
-    if row['Death- In Hospital'] == 1 or row['Death within 30 days of discharge'] == 1 or row['Arrhythmia that required treatment'] == 1 or row['Inotropes'] == 1 or row['Pressors'] == 1 or row["NRB (non-rebreather mask required)"] == 1 or row['ICU Stay'] == 1 or row['Shock (BP <90)'] == 1 or row['ACLS'] == 1 or row['Intubation'] == 1 or row['tpA'] == 1 or row['Transfusion'] == 1 or row['EKOS'] == 1:
+    if row['Death- In Hospital'] == 1 or row['Death within 30 days of discharge'] == 1 or \
+        row['Arrhythmia that required treatment'] == 1 or row['Inotropes'] == 1 or \
+        row['Pressors'] == 1 or row["NRB (non-rebreather mask required)"] == 1 or \
+        row['Shock (BP <90)'] == 1 or row['ACLS'] == 1 or \
+        row['Intubation'] == 1 or row['tpA'] == 1 or row['Transfusion'] == 1 or \
+        row['EKOS'] == 1:
         adverse_event = 1
     return adverse_event
 df_saddle['adverseEvent'] = df_saddle.apply(lambda row: adverse_event(row), axis=1)
@@ -48,12 +63,13 @@ df_nosaddle['adverseEvent'] = df_nosaddle.apply(lambda row: adverse_event(row), 
 # Combine Dataframes
 df_saddle['type'] = 'saddle'
 df_nosaddle['type'] = 'non-saddle'
-
+print(df_saddle.head())
+df_saddle = df_saddle.drop(columns=['ID']).reset_index(drop=True)
+df_nosaddle = df_nosaddle.drop(columns=['ID']).reset_index(drop=True)
+print(df_saddle.head())
 df_all = pd.concat([df_saddle, df_nosaddle], ignore_index=True)
 
 # Save clean data
-df_saddle.to_csv('saddle.csv', index=False)
-df_nosaddle.to_csv('nosaddle.csv', index=False)
-df_all.to_csv('all_types.csv', index=False)
-print('Files Successfully Saved...')
+df_all.to_csv('./objects/all_types.csv', index=False)
+print('File Successfully Saved...')
 
